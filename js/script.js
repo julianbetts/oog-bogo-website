6184c107cbf63bd47355ad545ada7d7f0493756f
Julian Betts <wjulianbetts@gmail.com>
2025-07-30 11:22:59 -0700

    fixed navbar got rid of here photo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const responseiveMenuButton = document.getElementById('responsiveToggleMenuButton')
+document.addEventListener('DOMContentLoaded', () => {
+    const responsiveMenuToggleButton = document.getElementById('responsiveMenuToggleButton')
+    const navbarLinks = document.querySelector('.navbar-links')
+
+    responsiveMenuToggleButton.addEventListener('click', () => {
+        responsiveMenuToggleButton.classList.toggle('open');
+        navbarLinks.classList.toggle('show');
+    })
+})
